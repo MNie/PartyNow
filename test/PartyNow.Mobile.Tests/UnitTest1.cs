4b12df0928784178e44617b06de717d07425ddfb
Michał Niegrzybowski <Micha Niegrzybowski>
2016-04-24 14:33:02 +0200

    add tests solution to windowsphone app

---
+++ test/PartyNow.Mobile.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace PartyNow.Mobile.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
