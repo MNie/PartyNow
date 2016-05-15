c340fbbbdc37fce111a4b41faa273a3c95148d47
michal.niegrzybowski@gmail.com <michal.niegrzybowski@gmail.com>
2016-05-16 00:01:38 +0200

    add styles and pass data to comboboxes in events tab

--- src/PartyNow.Mobile/PartyNow.Mobile.WindowsPhone/Views/Events.xaml.cs
+++ src/PartyNow.Mobile/PartyNow.Mobile.WindowsPhone/Views/Events.xaml.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Windows.Phone.UI.Input;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
-using PartyNow.DataContract.Models;
 using PartyNow.DataContract.Service;
 using PartyNow.Mobile.Common;
 
@@ -35,11 +35,16 @@ namespace PartyNow.Mobile.Views
         {
             var query = e.Parameter as string;
             var data = await GetData(query);
+            TypeFilterCombobox.Items?.Add(typeof(DataContract.Models.Events).GetRuntimeProperties());
+            TypeFilterCombobox.SelectionChanged += (sender, args) =>
+            {
+                TypeValueFilterCombobox.Items?.Add(
+                    data.Select(x => x.GetType().GetRuntimeProperty(args.AddedItems.First().ToString()).GetValue(x)));
+            };
             foreach (var @event in data.OrderBy(x => DateTime.Parse(x.startDate)))
             {
                 ResultsListBox.Items?.Add(@event);
             }
-
         }
 
         private async Task<IList<DataContract.Models.Events>> GetData(string parameter)
